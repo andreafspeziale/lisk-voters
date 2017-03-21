325626c3a9fe228d4d318da462ca75713851c07a
Davide Gessa <gessadavide@gmail.com>
2017-03-21 10:31:28 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 var votersApp = angular.module ('votersApp', []);
 
 votersApp.controller('indexController', function indexController($scope, $http) {
+    $scope.delegate = { you: '', other: '' };
+    $scope.loading = 'no';
+    $scope.node = '';
 
+    $scope.inspect = function () {
+        $scope.loading = 'yes';
+
+        //'/api/delegates/get?username=' + $scope.delegate.you
+    };
 });
