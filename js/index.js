78ffe7538965600398d61569b2ba0015b72b0e99
andreafspeziale <andreaf.speziale@gmail.com>
2017-03-21 10:19:30 +0100

    minimal layout

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 var votersApp = angular.module ('votersApp', []);
 
 votersApp.controller('indexController', function indexController($scope, $http) {
+    $scope.lignode = "liskwallet.punkrock.me"
+    $scope.address = ""
+    $scope.addressToCompare = ""
 
+    // $scope.checkDiff =  function() => {
+    //     if($scope.addressToCompare == "" && $scope.address == "") {
+    //
+    //     } else {
+    //         console.log($scope.address, $scope.addressToCompare)
+    //     }
+    // }
 });
