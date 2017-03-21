15a52f63441f6819ef9829630cb479ce0056d502
Davide Gessa <gessadavide@gmail.com>
2017-03-21 15:06:23 +0100

    Vote diff

--- js/index.js
+++ js/index.js
@@ -20,7 +20,13 @@ votersApp.controller('indexController', function indexController($scope, $http) 
                 $http.get ($scope.node + '/api/accounts/getBalance?address=' + data.data.delegate.address).then (
                     function (data2) {
                         data.data.delegate.balance = data2.data.balance / 100000000;
-                        return handler (data.data.delegate);
+
+                        $http.get ($scope.node + '/api/delegates/voters?publicKey=' + data.data.delegate.publicKey).then (
+                            function (data3) {
+                                data.data.delegate.voters = data3.data.accounts;
+                                return handler (data.data.delegate);
+                            }
+                        );
                     }
                 );
             }
@@ -34,6 +40,29 @@ votersApp.controller('indexController', function indexController($scope, $http) 
             $scope.data.you = data;
             updateDelegate ($scope.delegate.other, function (data) {
                 $scope.data.other = data;
+
+                $scope.notshared = [];
+
+                /* Compute the diff */
+                var mapped = {};
+                for (var i = 0; i < $scope.data.other.voters.length; i++) {
+                    var el = $scope.data.other.voters[i];
+                    mapped [el.username || el.address] = el;
+                }
+
+                for (var i = 0; i < $scope.data.you.voters.length; i++) {
+                    var el = $scope.data.you.voters[i];
+                    var k = el.username || el.address;
+                    if (k in mapped) {
+                        delete mapped[k];
+                    }
+                }
+
+                for (var k in mapped) {
+                    $scope.notshared.push (mapped[k]);
+                }
+                $scope.notshared.sort (function (a, b) { return b.balance - a.balance; });
+
                 $scope.loading = 'completed';
             });
         });
